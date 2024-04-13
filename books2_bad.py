3f8d318fa8990649dcca45932b3f807924e54c24
pattycheng <pattycheng@pattychengdeMacBook-Air.local>
2024-04-13 11:31:28 +0800

    add lesson 6

--- books2_bad.py
+++ books2_bad.py
@@ -1,9 +1,11 @@
 from typing import Optional
-from fastapi import FastAPI, Body, Path, Query, HTTPException
+from fastapi import FastAPI, Path, Query, HTTPException
 from starlette import status
 # Path用於定義路徑參數, Query 用於定義查詢參數
 from pydantic import BaseModel, Field
 # pydantic定義數據模型，然後使用這些模型來驗證和處理輸入的數據
+
+# 加了published_date後開始異常
 
 app = FastAPI()
 class Book:
@@ -12,21 +14,26 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published = published_date
 
 class BookRequest(BaseModel):
     #繼承BaseModel
-    id: Optional[int] = Field(title = "id is not needed")
+    id: Optional[int] = Field(title = "id is not needed") #沒傳入id，仍可自動生成這行，文字出不來
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6) # 0~5分，大於0小於6
+    published_date: int = Field(gt=1999, lt=2031)
+    # gt means greater than(大於)
+    # lt means less than(小於)
 
     class Config:
         json_schema_extra = {
@@ -35,16 +42,17 @@ class BookRequest(BaseModel):
                'author': 'codingwithruby',
                'description': 'computer science description',
                'rating': 5,
+               'published_date': 2022
             }
         }
 
 BOOKS = [
-    Book(1, 'computer science', 'Tim', 'computer science description', 5),
-    Book(2, 'math', 'Amy', 'math description', 5),
-    Book(3, 'chinese', 'Lulu', 'chinese description', 4),
-    Book(4, 'English', 'John', 'English description', 2),
-    Book(5, 'physics', 'Linda', 'physics description', 3),
-    Book(6, 'biology', 'Patty', 'biology description', 2)
+    Book(1, 'computer science', 'Tim', 'computer science description', 5, 2011),
+    Book(2, 'math', 'Amy', 'math description', 5, 2022),
+    Book(3, 'chinese', 'Lulu', 'chinese description', 3, 2011),
+    Book(4, 'English', 'John', 'English description', 2, 2000),
+    Book(5, 'physics', 'Linda', 'physics description', 1, 2003),
+    Book(6, 'biology', 'Patty', 'biology description', 5, 2004)
 ]
 
 @app.get("/books", status_code=status.HTTP_200_OK)
@@ -69,13 +77,13 @@ async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     return books_to_return
 
 # 書的出版日期
-# @app.get("/books/publish/")
-# async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
-#     books_to_return = []
-#     for book in BOOKS:
-#         if book.published == published_date:
-#             books_to_return.append(book)
-#     return books_to_return
+@app.get("/books/publish/")
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published == published_date:
+            books_to_return.append(book)
+    return books_to_return
 
 @app.post("/create-book")
 async def create_book(book_requset: BookRequest): #body會顯示預設內容
@@ -83,7 +91,7 @@ async def create_book(book_requset: BookRequest): #body會顯示預設內容
     BOOKS.append(find_book_id(new_book))
     # BOOKS.append(book_requset)
     # print(type(new_book)) #books2.Book
-    return BOOKS
+    # return BOOKS
 
 def find_book_id(book: Book):
     #另一種寫法-三元運算式: book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id+1
@@ -94,16 +102,16 @@ def find_book_id(book: Book):
     
     return book
 
+# 壞掉了
 @app.put('/books/update_book/', status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
-    book_changed = False # 是否有修改
+    # book_changed = False # 是否有修改
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
-            book_changed = True # 有修改
-        return BOOKS           
-    if not book_changed:
-        raise HTTPException(status_code=404, detail="Item not found")
+            # book_changed = True # 有修改
+    # if not book_changed:
+    #     raise HTTPException(status_code=404, detail="Item not found")
 
 # Path的目的是限制book_id必須大於0
 @app.delete("/books/{book_id}")
